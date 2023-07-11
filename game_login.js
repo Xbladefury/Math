6054a5a45cc12eb83deb5452f9aa3dcbef298b49
Xbladefury <101156964+Xbladefury@users.noreply.github.com>
2023-07-11 12:45:33 -0500

    Delete game_login.js

--- game_login.js
+++
@@ -1,7 +0,0 @@
-function adduser(){
-    player1name=document.getElementById("player1_name_input").value;
-    player2name=document.getElementById("player2_name_input").value;
-    localStorage.setItem("Player1 Name",player1name);
-    localStorage.setItem("Player2 Name",player2name);
-    window.location="game_page.html";
-    }
